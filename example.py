a321d507817919a0eee60228f36ada10e1372db8
Léopold Le Roux <lerouxl@cardiff.ac.uk>
2021-12-02 20:28:08 +0000

    Simple example

---
+++ example.py
@@ -0,0 +1,16 @@
+from ARC_CSV import Arc_reader
+import polyscope as ps
+
+# Create an arc reader object
+arc = Arc_reader()
+
+# Read a csv dump
+arc.load_csv("examples/95428XstlXremeshedX_FV_part_13.csv")
+
+# Extract the point cloud coordinate
+arc.get_coordinate()
+
+# Add at each point all extract data
+arc.get_point_cloud_data()
+
+ps.show()
