1d6dac4bb8a82c81764403156c272091e01bdfe0
Léopold <36997474+lerouxl@users.noreply.github.com>
2022-08-04 10:00:57 +0200

    Add pip setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SIMUFACT_ARC_READER",
+    version="1.0.0",
+    author="Léopold Le Roux",
+    author_email="",
+    description="A python reader for Simufact ARC file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lerouxl/Simufact_ARC_reader",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",  
+        "Operating System :: OS Independent",
+    ),
+)
